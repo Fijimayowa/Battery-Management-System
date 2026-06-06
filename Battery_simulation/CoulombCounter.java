b5165bd03aa329892ad8996c2bc92b4f36ebfb76
Fijimayowa <femiijimayowa@gmail.com>
2026-06-06 16:25:08 -0400

    lol

---
+++ Battery_simulation/CoulombCounter.java
@@ -0,0 +1,29 @@
+package Battery_simulation;
+
+import java.util.Map;
+
+public class CoulombCounter extends VoltageReader {
+    double SoC = super.SoC();
+    int curr;
+    int batteryCapacity;
+    float lol;
+
+    CoulombCounter(double SoC, int curr, int batteryCapacity) {
+        this.SoC = SoC;
+        this.curr = curr;
+        this.batteryCapacity = batteryCapacity;
+        System.out.println();
+    }
+
+    private float kalmanFilter(){
+        float uncertaintyGain=;
+
+    }
+
+    static public float CoulombCount(double bC, int curr) {
+        double totalVolt = super.VoltageReader().sum();
+        double res = SoC + (1 / bC) + curr;
+        return res;
+
+    }
+}
