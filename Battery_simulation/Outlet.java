0ddcb7c0bf8fe2134a5cee10c89d61d3078082a9
Fijimayowa <femiijimayowa@gmail.com>
2026-06-25 13:46:45 -0400

    June 25

---
+++ Battery_simulation/Outlet.java
@@ -0,0 +1,42 @@
+package Battery_simulation;
+
+public class Outlet {
+    boolean switch;
+    float current, voltage, resistance;
+
+void plugIn(){
+    System.out.println("lol");
+}
+
+void unplug()
+
+void turnOn()
+
+void turnOff()
+
+boolean isOccupied()
+
+boolean isPowerOn()
+
+float getVoltage()
+
+float getCurrent()
+
+float getPower()
+
+boolean hasGround()
+
+boolean isGrounded()
+
+void tripBreaker()
+
+void resetBreaker()
+
+String getConnectedDevice()
+
+String setConnectedDevice()
+
+boolean isCircuitComplete()
+    
+    
+}
