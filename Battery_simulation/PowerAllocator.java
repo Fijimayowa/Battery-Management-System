3e69ad5a38be83c514952267a213f24cdf5503ec
Fijimayowa <femiijimayowa@gmail.com>
2026-06-28 17:27:01 -0400

    June 28

--- Battery_simulation/PowerAllocator.java
+++ Battery_simulation/PowerAllocator.java
@@ -1,5 +1,25 @@
 package Battery_simulation;
 
+import java.util.Queue;
+
 public class PowerAllocator {
+    LightBulb light;
+    Fan fan;
+    Outlet outlet;
+    Radio radio;
+    BMSObjects pirority = null;
+    int pirority;
+
+    PowerAllocator(LightBulb lt, Fan fn, Outlet ot, Radio rd) {
+        light = lt;
+        fan = fn;
+        outlet = ot;
+        radio = rd;
+
+    }
+
+    public void Queue(BMSObjects variable) {
+
+    }
 
 }
