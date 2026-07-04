e6f42f3a216a1d2ac369e89b0d10d96f4f881e39
Fijimayowa <femiijimayowa@gmail.com>
2026-07-03 21:54:19 -0400

    july3

--- python_files/PID_Tempetute_Control.py
+++ python_files/PID_Tempetute_Control.py
@@ -1,3 +1,15 @@
 import mathplotlib.pyplot as plt
 import numpy as np
+from flask import Flask, render_template, flash, redirect, url_for, request, jsonify
 
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def index(method=["GET", "POST"]):
+    np._IntegralArrayT
+
+
+if __name__=="__main__":
+    app.debug(True)
