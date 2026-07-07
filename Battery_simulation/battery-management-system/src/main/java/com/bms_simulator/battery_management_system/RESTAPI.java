397af8640dd31dec98ebf31b44592ccba0f4fe79
Fijimayowa <femiijimayowa@gmail.com>
2026-07-06 23:48:35 -0400

    July 6

--- Battery_simulation/battery-management-system/src/main/java/com/bms_simulator/battery_management_system/RESTAPI.java
+++ Battery_simulation/battery-management-system/src/main/java/com/bms_simulator/battery_management_system/RESTAPI.java
@@ -1,6 +1,24 @@
-package Battery_simulation;
+package com.bms_simulator.battery_management_system;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class RESTAPI {
+
+    @GetMapping("/getReading")
+    public String getData(@RequestParam String param) {
+        return new String();
+    }
+
+    @PostMapping("/sendReading")
+    public float[][] postData(@RequestBody float[][] entity) {
+        // TODO: process POST request
+        System.out.println(entity);
+        return entity;
+    }
 
 }
