0f45687997d1bd31c5f716eef19724b1370b4c98
Fijimayowa <femiijimayowa@gmail.com>
2026-09-01 18:40:39 -0400

    Sept 2 pt 3

---
+++ my-app/src/assets/Car_interface.jsx
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+
+
+function App(){
+    return(
+        <>
+        <div id='Home'><div>
+            My Leviton Home</div>
+            </div>
+        </>
+    )
+}
