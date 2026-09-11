76bc87454628c0066640b13d2d29e744e1ff176d
Fijimayowa <femiijimayowa@gmail.com>
2026-09-11 17:42:23 -0400

    911

--- Thermal_PIDController/Thermal_PIDController.c
+++ Thermal_PIDController/Thermal_PIDController.c
@@ -17,6 +17,12 @@
 #define INA219_ADDR 0x40
 
 #define R_SHUNT 0.01
+
+#define Battery_adc 26
+#define Radio_adc 27
+#define Lightbulb 28
+#define CoolingFan 29
+
 void poll_voltage()
 {
     i2c_init();
@@ -27,17 +33,24 @@ void poll_voltage()
     i2c_read_register(INA219_ADDR, Bus_voltage_register, &bus_voltage);
 }
 
-void thermal_model(){
-    
-}
+void thermal_model(int device)
+{
+    adc_gpio_init(device);
 
+}
 int main()
 {
     stdio_init_all();
+    adc_init();
     gpio_set_function(SDA_pin, GPIO_FUNC_I2C);
     gpio_set_function(SDL_pin, GPIO_FUNC_I2C);
     double expected_current = 600, calbration = expected_current / 32768;
     double real_calbration = 0.04096 / (calbration * R_SHUNT);
+    uint8_t radio, fan, light;
+    adc_init();
+    adc_gpio_init(26); // Radio
+    adc_gpio_init(27); // LightBulb
+    adc_gpio_init(28); // CoolingFan
     if (expected_current < 1)
     {
         expected_current = 1d;
